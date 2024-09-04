f699b2d338ef01499ddd193ede222f7cdc728e52
YoussefSayeddd <youyousayed5@gmail.com>
2024-09-04 18:36:42 +0200

    REsolved

--- src/App.js
+++ src/App.js
@@ -13,11 +13,25 @@ function App() {
         setCoordinates({ latitude, longitude });
       },
       (error) => {
-        console.error('Error fetching location:', error);
+        console.error('Error fetching location:', error.message);
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.error("User denied the request for Geolocation.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            console.error("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            console.error("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            console.error("An unknown error occurred.");
+            break;
+        }
       }
     );
-    console.log(position.coords);
   }, []);
+  
 
   return (
     <Router>
